ed319c99484f2578a8e5d77785b51f035b629f71
Varun Selvakumar <varunselvakumar6@gmail.com>
2025-11-04 10:03:01 +0530

    na

--- script.js
+++ script.js
@@ -1,58 +1,25 @@
 const sentences = [
   "The quick brown fox jumps over the lazy dog.",
   "JavaScript makes web pages interactive and dynamic.",
-  "Coding is like solving a puzzle with logic and patience."
+  "Coding is like solving a puzzle with logic and patience.",
+  "The quick brown fox jumps over the lazy dog.",
+  "JavaScript makes web pages interactive and dynamic.",
+  "Coding is like solving a puzzle with logic and patience.",
+  "The quick brown fox jumps over the lazy dog.",
+  "JavaScript makes web pages interactive and dynamic.",
+  "Coding is like solving a puzzle with logic and patience.",
+  "The quick brown fox jumps over the lazy dog.",
+  "JavaScript makes web pages interactive and dynamic.",
+  "Coding is like solving a puzzle with logic and patience.",
+  "The quick brown fox jumps over the lazy dog.",
+  "JavaScript makes web pages interactive and dynamic.",
+  "Coding is like solving a puzzle with logic and patience.",
+  "The quick brown fox jumps over the lazy dog.",
+  "JavaScript makes web pages interactive and dynamic.",
+  "Coding is like solving a puzzle with logic and patience.",
 ];
 document.getElementById("sentence").innerText=sentences;
 document.getElementById("input").disabled=true;
-
-
-
-//  function runTimer(){
-//         	sec--;
-//             document.getElementById("demo").innerHTML = ` ${min}: ${sec}`
-// 	            if(sec===0)
-// 	        {
-// 	            sec = 5;
-// 	            min --;
-// 	        }
-// 	        if (min<0){
-// 	        	clearInterval(timer)
-// 	        	document.getElementById("demo2").innerHTML = ` Time Up`
-// 	        }
-//         }
-//         timer = setInterval(runTimer, 1000);
-let min=0;
-let sec=5
-let options=document.getElementById("fruits")
-function startGame() {
-    document.getElementById("input").disabled = false;
-    document.getElementById("input").value = ""; // clear previous input
-
-    let sec = 5; // seconds per round
-    let optionsValue = parseInt(options.value); // total rounds
-
-    const timing = setInterval(() => {
-        document.getElementById("time").innerHTML = `${optionsValue}: ${sec}`;
-        sec--;
-
-        if (sec < 0) {
-            optionsValue--; // next round
-            if (optionsValue < 0) {
-                clearInterval(timing); // stop timer exactly
-                document.getElementById("time").innerHTML = `Time's up!`;
-
-                // call score after timer completes
-                score();
-                document.getElementById("input").disabled = true;
-               
-            }
-            sec = 5; // reset seconds for next round
-        }
-    }, 1000);
-}
-
-
 
 function score() {
     const ipvalue = document.getElementById("input").value.trim();
@@ -70,7 +37,7 @@ function score() {
     } 
 
     const accuracy = Math.round((accurate / sentence.length) * 100);
-    document.getElementById("accuracy").innerText = `Accuracy: ${accuracy}`;
+    document.getElementById("accuracy").innerText = `${accuracy}`;
     console.log("Accuracy:", accuracy);
 
     const len = ipvalue.length;
@@ -81,3 +48,37 @@ function score() {
     document.getElementById("wpm").innerText = `Speed: ${wpm} `;
   
 }
+
+
+
+let options=document.getElementById("fruits")
+function startGame() {
+    document.getElementById("input").disabled = false;
+    document.getElementById("input").value = "";
+    document.getElementById("startBtn").disabled=true;
+    let sec = 60; // seconds per round
+    let optionsValue = parseInt(options.value); // total rounds
+
+    const timing = setInterval(() => {
+        document.getElementById("time").innerHTML = `${optionsValue-1}: ${sec}`;
+        sec--;
+
+        if (sec < 0) {
+            optionsValue--; // next round
+            if (optionsValue < 0) {
+                clearInterval(timing); // stop timer exactly
+                document.getElementById("time").innerHTML = `Time's up!`;
+                 
+                // call score after timer completes
+                score();
+                document.getElementById("input").disabled = true;
+                document.getElementById("startBtn").disabled=false;
+               
+            }
+            sec = 60; // reset seconds for next round
+        }
+    }, 1000);
+}
+
+
+
